183878d7ec4788decb209a3840e436fc8719b43b
Kormi9722 <kormi9719@gmail.com>
2020-11-07 18:11:01 +0100

    Default upload

---
+++ src/main/java/hu/elte/fullstack/bevasarlolista/BevasarlolistaApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.fullstack.bevasarlolista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BevasarlolistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BevasarlolistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/elte/fullstack/bevasarlolista/BevasarlolistaApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.elte.fullstack.bevasarlolista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BevasarlolistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
