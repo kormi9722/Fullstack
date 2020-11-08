000419385dff18077ccaba15af5a6b68cd777daa
TheHagen98 <61209594+TheHagen98@users.noreply.github.com>
2020-11-08 12:29:36 +0100

    Update BevasarloLista.java
    List import add

--- src/main/java/hu/elte/fullstack/bevasarlolista/entities/BevasarloLista.java
+++ src/main/java/hu/elte/fullstack/bevasarlolista/entities/BevasarloLista.java
@@ -3,6 +3,7 @@ package hu.elte.fullstack.bevasarlolista.entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class BevasarloLista {
 
