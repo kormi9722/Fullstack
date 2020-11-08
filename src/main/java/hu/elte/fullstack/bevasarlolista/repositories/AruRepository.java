2607ba144f0a61272b311e86da45e2860d198aad
TheHagen98 <61209594+TheHagen98@users.noreply.github.com>
2020-11-08 13:08:31 +0100

    Update AruRepository.java

--- src/main/java/hu/elte/fullstack/bevasarlolista/repositories/AruRepository.java
+++ src/main/java/hu/elte/fullstack/bevasarlolista/repositories/AruRepository.java
@@ -2,6 +2,7 @@ package hu.elte.fullstack.bevasarlolista.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import hu.elte.fullstack.bevasarlolista.entities.Aru;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AruRepository extends CrudRepository<Aru, Integer>{
