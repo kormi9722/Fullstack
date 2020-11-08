cd2c035b9c872ba5d1013e4fd271e9ffb5d8bd48
Kormi9722 <kormi9719@gmail.com>
2020-11-08 17:39:41 +0100

    ManyToMany added BevasarloLista Error

---
+++ src/main/java/hu/elte/fullstack/bevasarlolista/entities/BevasarloLista.java
@@ -0,0 +1,32 @@
+package hu.elte.fullstack.bevasarlolista.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "BEVASARLOLISTA")
+public class BevasarloLista {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer sorszam;
+
+    @ManyToOne
+    @JoinColumn
+    private Felhasznalo felhasznalo;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "bevasarloListak")
+    private List<Aru> aruk;
+
+}
